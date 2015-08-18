48ba56ce2762b446cf969dcacc85bdbfa6cdc1c6
Jan Verhoeven <jan@visity.nl>
2015-08-18 20:09:00 +0200

    Renamed environment variables to CARBON.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 #Usage
 This docker sends a number of system metrics to a configurable graphite instance.
 
-This docker does not use linking because chances are substantial the graphite runs on a different host. As such environment variables are supported. When using Maestro-ng this is done automatically as long as the graphite service is named 'graphite' and the instance 'gp1' and the port 'line\_receiver'
+This docker does not use linking because chances are substantial the graphite runs on a different host. As such environment variables are supported. When using Maestro-ng this is done automatically as long as the graphite service is named 'carbon' and the instance 'carbon1' and the port 'line\_receiver'
 
 If Maestro is not used, these variables are mandatory:
-* GRAPHITE\_GP1\_HOST: The host to which to send the metrics to (via line receiver)
-* GRAPHITE\_LINE\_RECEIVER\_PORT: The port on which to send the metrics to.
+* CARBON\_HOST: The host to which to send the metrics to (via line receiver)
+* CARBON\_LINE\_RECEIVER\_PORT: The port on which to send the metrics to.
 
 Example:
 
-	docker run -d -e GRAPHITE_GP1_HOST=10.0.0.3 -e GRAPHITE_LINE_RECEIVER_PORT=2003 visity/monitor-host
+	docker run -d -e CARBON_HOST=10.0.0.3 -e CARBON_LINE_RECEIVER_PORT=2003 visity/monitor-host
--- set_graphite_endpoint.sh
+++ set_graphite_endpoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-if [ -z "$GRAPHITE_HOST" ]; then
-	if [ -n "$GRAPHITE_GP1_HOST" ]; then
-		GRAPHITE_HOST=`echo "$GRAPHITE_GP1_HOST"`
+if [ -z "$CARBON_HOST" ]; then
+	if [ -n "$CARBON_CARBON1_HOST" ]; then
+		CARBON_HOST=`echo "$CARBON_CARBON1_HOST"`
 	fi
 fi
 
-if [ -z "$GRAPHITE_LINE_RECEIVER_PORT" ]; then
-	if [ -n "$GRAPHITE_GP1_LINE_RECEIVER_PORT" ]; then
-		GRAPHITE_LINE_RECEIVER_PORT=`echo "$GRAPHITE_GP1_LINE_RECEIVER_PORT"`
+if [ -z "$CARBON_LINE_RECEIVER_PORT" ]; then
+	if [ -n "$CARBON_CARBON1_LINE_RECEIVER_PORT" ]; then
+		CARBON_LINE_RECEIVER_PORT=`echo "$CARBON_CARBON1_LINE_RECEIVER_PORT"`
 	fi
 fi
 
-if [ -n "$GRAPHITE_HOST" ]; then
-   sed 's/{{graphite_host}}/'"$GRAPHITE_HOST"'/' -i /etc/diamond/diamond.conf;
+if [ -n "$CARBON_HOST" ]; then
+   sed 's/{{CARBON_host}}/'"$CARBON_HOST"'/' -i /etc/diamond/diamond.conf;
 else
-	echo "Diamond: No GRAPHITE_HOST or GRAPHITE_GP1_HOST defined. Aborting.";
+	echo "Diamond: No CARBON_HOST or CARBON_CARBON1_HOST defined. Aborting.";
 	exit 1;
 fi
 
-if [ -n "$GRAPHITE_LINE_RECEIVER_PORT" ]; then
-  sed 's/{{graphite_line_receiver_port}}/'"$GRAPHITE_LINE_RECEIVER_PORT"'/' -i /etc/diamond/diamond.conf;
+if [ -n "$CARBON_LINE_RECEIVER_PORT" ]; then
+  sed 's/{{CARBON_line_receiver_port}}/'"$CARBON_LINE_RECEIVER_PORT"'/' -i /etc/diamond/diamond.conf;
 else
-	echo "Diamond: No GRAPHITE_LINE_RECEIVER_PORT or GRAPHITE_GP1_LINE_RECEIVER_PORT defined. Aborting.";
+	echo "Diamond: No CARBON_LINE_RECEIVER_PORT or CARBON_CARBON1_LINE_RECEIVER_PORT defined. Aborting.";
 	exit 1;
 fi
